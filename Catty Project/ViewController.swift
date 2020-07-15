8a8e394bbf44da4322429d56d5ccb2a8b604a841
Sergey Korshunov <79185660148@ya.ru>
2020-07-15 10:04:29 +0300

    Initial Commit

---
+++ Catty Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Catty Project
+//
+//  Created by Сергей Коршунов on 15.07.2020.
+//  Copyright © 2020 Sergey Korshunov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

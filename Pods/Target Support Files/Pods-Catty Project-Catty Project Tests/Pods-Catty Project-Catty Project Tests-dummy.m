63e90a874cd44073331a807c9e0121a0dd232055
Sergey Korshunov <79185660148@ya.ru>
2020-08-03 11:11:05 +0300

    .gitignore is now working

--- Pods/Target Support Files/Pods-Catty Project-Catty Project Tests/Pods-Catty Project-Catty Project Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Catty_Project_Catty_Project_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_Catty_Project_Catty_Project_Tests
-@end
